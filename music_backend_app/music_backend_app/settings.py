fc656d8f01f73baa290b7f7be2ff5e91f3b9c35a
sreekuttyma22-hash <sreekuttyma22@gmail.com>
2026-09-16 12:58:35 +0530

    Configure static files for deployment

--- music_backend_app/music_backend_app/settings.py
+++ music_backend_app/music_backend_app/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 
 # Email
@@ -144,7 +144,8 @@ MAILERS = {
     },
 }
 
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
